5b55a07a4906d5d7457204b54cd8dccb6172e796
Aruna <aruna.evan15@gmail.com>
2021-11-29 14:07:48 -0800

    commit tree 6e8875857686ced6aa9f90e514f5117919af298f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    newItem = Item.create(item_params)
+    user.items << newItem
+    user.save
+    render json: newItem, include: :user, status: :created
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
